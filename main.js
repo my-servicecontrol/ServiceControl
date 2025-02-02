5ff93ffc1ce6ba281b81dc25288b3ba873051882
my-servicecontrol <148506838+my-servicecontrol@users.noreply.github.com>
2025-02-02 13:07:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ var tasks = "1tUkWfP-Ci68M-bh4nsEI0VxlOoEvvNv64fhwhwivNCU";
 var sName = "Service Control";
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-	$("#dateend").html('Email: ');
+	$("#dateend").html('Email: ' + profile);
 }  
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      $("#dateend").html('EmailA: ');
+      $("#dateend").html('EmailA: ' + auth2);
     });
   }
 
