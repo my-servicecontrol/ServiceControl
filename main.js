10a09d587f2497469716dc143f3e0583f6595357
my-servicecontrol <148506838+my-servicecontrol@users.noreply.github.com>
2025-02-02 04:11:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var myApp =
   "https://script.google.com/macros/s/AKfycbwoV5nA0B9PWVWOUL0dZn5qA5aKCBPkVYHyCQA0AEFgeVuTA2xfIlSfV36Vs4wt6Iiq/exec";
 var tasks = "1tUkWfP-Ci68M-bh4nsEI0VxlOoEvvNv64fhwhwivNCU";
 var sName = "Service Control";
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+	$("#dateend").html('Email: ' + profile.getEmail());
+}
 //var eDate = "Активно до: 18.08.2024";
 $("#offcanvasNavbarLabel").html(sName);
 //$("#dateend").html(eDate);
