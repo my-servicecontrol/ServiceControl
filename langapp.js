133143b7135d746ac1648d91e535cbdc4a31960d
my-servicecontrol <148506838+my-servicecontrol@users.noreply.github.com>
2025-08-19 08:37:40 +0300

    Update langapp.js

--- langapp.js
+++ langapp.js
@@ -1,12 +1,22 @@
-select.addEventListener("change", changeURLLanguage);
-function changeURLLanguage() {
-  // Получаем выбранный язык
-  let lang = select.value;
-  // Проверяем, есть ли параметр wlink из main и формируем новый URL
-  let newURL = wlink
-    ? `${window.location.pathname}?${wlink}#${lang}`
-    : `${window.location.pathname}#${lang}`;
-  // Меняем URL и перезагружаем страницу
-  location.href = newURL;
-  document.querySelector("title").innerHTML = langArr["unit"][lang];
+// Функция обновления перевода
+function changeLanguage(lang) {
+  var selected = document.querySelector(".change-lang");
+  //document.querySelector("title").innerHTML = langArr["unit"][lang];
+  history.replaceState(null, null, `${window.location.pathname}#${lang}`);
+  localStorage.setItem("appLang", lang);
+  selected.value = lang;
+
+  for (let key in langArr) {
+    let elem = document.querySelector(".lng-" + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][lang];
+    }
+  }
 }
+var select = document.querySelector(".change-lang");
+// Обработчик смены языка
+select.addEventListener("change", function () {
+  let newLang = select.value;
+  // Обновляем перевод
+  changeLanguage(newLang);
+});
