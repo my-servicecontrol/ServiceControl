f0b93660071c1d204743667bfc36330b42dc8f47
my-servicecontrol <148506838+my-servicecontrol@users.noreply.github.com>
2025-09-09 10:54:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var allLang = ["ua", "ru", "en", "de", "es"];
 // язык из hash
 const hashLang = window.location.hash.substr(1);
 var myApp =
-  "https://script.google.com/macros/s/AKfycbz2jnjfhKohWVbWPS8A9mfVmmxnror9jMc2MJK8gOYFgdAi53de4ijJaxnVrgOi6oRW/exec";
+  "https://script.google.com/macros/s/AKfycbzn0vQ8Dj2HtnNPY1FmC7EfQQaXU3_8H_eeqfmCd0BagsoKZ1KmZKCek0A70ys7HrqU/exec";
 var sName = "";
 var tasks = "";
 var price = "";
@@ -2244,3 +2244,4 @@ function getUserData(serverResponse) {
   if (serverResponse.success) {
   }
 }
+
