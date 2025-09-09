82dc960e33af2907946e86806553b85f6905ffbb
my-servicecontrol <148506838+my-servicecontrol@users.noreply.github.com>
2025-09-09 23:08:46 +0300

    Update langapp.js

--- langapp.js
+++ langapp.js
@@ -25,6 +25,7 @@ var select = document.querySelector(".change-lang");
 select.addEventListener("change", function () {
   let newLang = select.value;
   // Обновляем перевод
-  loadTasks();
   changeLanguage(newLang);
+  tasksTable();
 });
+
