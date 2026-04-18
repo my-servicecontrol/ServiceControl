b8717cd93b2a7308c6e3f5c849e188035bb05ddd
my-servicecontrol <148506838+my-servicecontrol@users.noreply.github.com>
2026-04-18 15:04:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2780,7 +2780,11 @@ function switchToInput(td, colIndex, saveCallback = saveChanges) {
           .split("/")
           .map((s) => s.trim())
           .filter(Boolean)
+.filter((name) => !name.startsWith("__"));
       : [];
+
+const archivedInCell = selectedVals.filter((v) => v.startsWith("__"));
+const activeSelectedInCell = selectedVals.filter((v) => !v.startsWith("__"));
 
     // --- создаём меню ---
     const menu = document.createElement("div");
@@ -2807,7 +2811,7 @@ function switchToInput(td, colIndex, saveCallback = saveChanges) {
       chk.id = `executor_chk_${baseTs}_${Math.random()
         .toString(36)
         .slice(2, 7)}`;
-      if (selectedVals.includes(exec)) chk.checked = true;
+if (activeSelectedInCell.includes(exec)) chk.checked = true;
 
       const lbl = document.createElement("label");
       lbl.className = "form-check-label ms-2 mb-0";
@@ -2948,7 +2952,8 @@ function switchToInput(td, colIndex, saveCallback = saveChanges) {
       ).map((c) => c.value);
       const manualVal = customInput.value.trim();
       if (manualVal) chosen.push(manualVal);
-      return chosen.filter(Boolean);
+        const finalArray = [...new Set([...archivedInCell, ...chosen])];
+  return finalArray.filter(Boolean);
     };
 
     addBtn.addEventListener("click", (e) => {
